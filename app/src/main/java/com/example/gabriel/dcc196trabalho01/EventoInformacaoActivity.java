13f070f866a2bce9dfe9aa5360467ff7e78de09c
Mateus Gonçalo <mateusgon57@gmail.com>
2018-11-15 13:48:11 -0200

    Alterando o EventoInformacaoActivity

--- app/src/main/java/com/example/gabriel/dcc196trabalho01/EventoInformacaoActivity.java
+++ app/src/main/java/com/example/gabriel/dcc196trabalho01/EventoInformacaoActivity.java
@@ -1,6 +1,8 @@
 package com.example.gabriel.dcc196trabalho01;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,6 +24,8 @@ public class EventoInformacaoActivity extends AppCompatActivity {
     private TextView vagasEvento;
     private TextView inscritosEvento;
     private TextView descricaoEvento;
+    private Cursor cursor;
+    private EventoDbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,27 +41,55 @@ public class EventoInformacaoActivity extends AppCompatActivity {
         descricaoEvento = findViewById(R.id.txtDescricaoEventoInformacao);
         rv_ListaParticipantesEvento = findViewById(R.id.rv_ListaDeInscritosEventoInformacao);
 
+        dbHelper = new EventoDbHelper(getApplicationContext());
+
+        Bundle extras = getIntent().getExtras();
 
         Intent intent = getIntent();
-        recuperado = (Evento) intent.getSerializableExtra("evento");
-        preencheInfos(recuperado);
+        Integer registro = extras.getInt("position");
+        preencheInfos(registro);
 
         rv_ListaParticipantesEvento.setLayoutManager(new LinearLayoutManager(this));
-        rv_ListaParticipantesEvento.setAdapter(new ParticipanteAdapter(recuperado.getParticipanteList()));
+        //rv_ListaParticipantesEvento.setAdapter(new ParticipanteAdapter(recuperado.getParticipanteList()));
 
     }
 
-    public void preencheInfos(Evento evento)
+    public void preencheInfos(Integer registro)
     {
-        nomeEvento.setText("Nome: " + evento.getNome());
-        localEvento.setText("Local: " + evento.getLocal());
-        Calendar c = evento.getDataEvento();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-        String data = sdf.format(c.getTime());
-        dataEvento.setText("Data: " + data);
-        facilitadorEvento.setText("Facilitador: " + evento.getFacilitador());
-        vagasEvento.setText("Vagas: " + String.valueOf(evento.getNumMaximoInscritos()));
-        inscritosEvento.setText("Inscritos: " + String.valueOf(evento.getNumInscritos()));
-        descricaoEvento.setText("Descrição: " + evento.getDescricao());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String []visao = {
+                AppContract.Evento.COLUMN_NAME_REGISTRO,
+                AppContract.Evento.COLUMN_NAME_NOME,
+                AppContract.Evento.COLUMN_NAME_LOCAL,
+                AppContract.Evento.COLUMN_NAME_DATA,
+                AppContract.Evento.COLUMN_NAME_FACILITADOR,
+                AppContract.Evento.COLUMN_NAME_DESCRICAO,
+                AppContract.Evento.COLUMN_NAME_INSCRITOS,
+                AppContract.Evento.COLUMN_NAME_MAXINSCRITOS,
+        };
+
+        String select = AppContract.Evento.COLUMN_NAME_REGISTRO+" = ?";
+        String [] selectArgs = {String.valueOf(registro)};
+
+        cursor = db.query(AppContract.Evento.TABLE_NAME, visao,select,selectArgs,null,null, null);
+
+        int idxNum = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_REGISTRO);
+        int idxNome = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_NOME);
+        int idxLocal = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_LOCAL);
+        int idxData = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_DATA);
+        int idxFacilitador = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_FACILITADOR);
+        int idxDescricao = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_DESCRICAO);
+        int idxInscritos = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_INSCRITOS);
+        int idxMaxInscritos = cursor.getColumnIndexOrThrow(AppContract.Evento.COLUMN_NAME_MAXINSCRITOS);
+
+        cursor.moveToPosition(0);
+
+        nomeEvento.setText("Nome: " + cursor.getString(idxNome));
+        localEvento.setText("Local: " + cursor.getString(idxLocal));
+        dataEvento.setText("Data: ");
+        facilitadorEvento.setText("Facilitador: " + cursor.getString(idxFacilitador));
+        vagasEvento.setText("Vagas: " + String.valueOf(cursor.getInt(idxMaxInscritos)));
+        inscritosEvento.setText("Inscritos: " + String.valueOf(cursor.getInt(idxInscritos)));
+        descricaoEvento.setText("Descrição: " + cursor.getString(idxDescricao));
     }
 }
